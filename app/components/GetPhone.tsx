ce69a400d23fafb4f32e9ef4fa3b56cb8ee3781b
Yuri Toledo <yuriwtoledo@gmail.com>
2023-09-10 12:34:47 -0300

    create GetPhone component

---
+++ app/components/GetPhone.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { ChangeEvent, FormEvent, useState } from "react"
+import { phoneLength, phoneMask } from "../utils/phone"
+import { Input } from "./Input"
+import { NextButton } from "./NextButton"
+import Title from "./Title"
+import { useMovingDispatcher } from "../store/useMovingDispatcher"
+import { ErrorMessage } from "../types/ErrorMessage"
+
+export default function GetPhone() {
+  const [value, setValue] = useState("")
+  const [errorMessage, setErrorMessage] = useState<ErrorMessage>("")
+  const { setPhone } = useMovingDispatcher()
+
+  const phoneValidator = (value: string) =>
+    value.length !== phoneLength ? "Please enter a valid phone number" : ""
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const errorMessage = phoneValidator(value)
+    if (errorMessage) {
+      setErrorMessage(errorMessage)
+      return
+    }
+
+    setErrorMessage(null)
+    setPhone(value)
+  }
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    if (value.length > phoneLength) return
+
+    setValue(phoneMask(event.target.value))
+  }
+
+  const onBlur = () => {
+    setErrorMessage(phoneValidator(value))
+  }
+
+  return (
+    <>
+      <Title>What is your phone number?</Title>
+
+      <form className="flex flex-col w-full" onSubmit={onSubmit}>
+        <Input
+          type="tel"
+          value={value}
+          onChange={onChange}
+          errorMessage={errorMessage}
+          onBlur={onBlur}
+        />
+
+        <NextButton />
+      </form>
+    </>
+  )
+}
