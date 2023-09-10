cc437b60268d6d28493f3347eb79f1f92a2999bc
Yuri Toledo <yuriwtoledo@gmail.com>
2023-09-10 14:55:45 -0300

    adding animation in the progress

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -4,18 +4,26 @@ import { useAppSelector } from "../store/hooks"
 const ProgressBar = () => {
   const step = useAppSelector(state => state.moving.step)
 
+  const containerClassName = clsx(
+    "flex justify-between w-full py-4 duration-500 delay-300 ease-in-out",
+    {
+      "opacity-0": step == 1,
+      "opacity-1": step > 1,
+    }
+  )
+
+  const progressBarClassName = clsx(
+    "h-3 rounded-full w-0 duration-700 delay-500 ease-in-out",
+    {
+      "w-1/2 bg-green-500": step === 2,
+      "w-full bg-green-700 animate-pulse": step === 3,
+    }
+  )
+
   return (
-    <div className="flex justify-between w-full">
-      <div className="w-full rounded-full h-2.5 bg-gray-200">
-        <div
-          className={clsx(
-            "bg-blue-600 h-2.5 rounded-full w-0 duration-500 ease-in-out delay-300",
-            {
-              "w-1/2": step === 2,
-              "w-full bg-green-600": step === 3,
-            }
-          )}
-        />
+    <div className={containerClassName}>
+      <div className="w-full rounded-full h-3 bg-gray-200">
+        <div className={progressBarClassName} />
       </div>
     </div>
   )
