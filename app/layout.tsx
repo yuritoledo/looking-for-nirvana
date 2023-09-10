c08c41abb036dd28b3e9e810f20aae1e1be2c965
Yuri Toledo <yuriwtoledo@gmail.com>
2023-09-10 12:34:28 -0300

    remove react namespace import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Providers } from "./store/provider"
+import { ReactNode } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,11 +11,7 @@ export const metadata: Metadata = {
   description: "Address search",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
