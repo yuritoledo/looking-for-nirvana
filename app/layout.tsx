700111cb8f1118e88cf0387b22cf548d4ea48017
Yuri Toledo <yuriwtoledo@gmail.com>
2023-09-10 14:55:57 -0300

    create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <nav className="flex justify-between items-center px-5 py-3 bg-blue-600">
+          <h1 className="text-2xl font-bold">
+            <span className="text-blue-300">Looking for</span>{" "}
+            <span className="text-white">Nirvana</span>
+          </h1>
+        </nav>
         <Providers>{children}</Providers>
       </body>
     </html>
