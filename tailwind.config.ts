d7f51da3118728e2ded6a6d7b28fb64161411aea
Yuri Toledo <yuriwtoledo@gmail.com>
2023-09-11 22:45:50 -0300

    increase translate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,11 +20,11 @@ const config: Config = {
           "100%": { opacity: "1" },
         },
         toLeft: {
-          "0%": { opacity: "0", transform: "translateX(15px)" },
+          "0%": { opacity: "0", transform: "translateX(30px)" },
           "100%": { opacity: "1", transform: "translateX(0px)" },
         },
         toRight: {
-          "0%": { opacity: "0", transform: "translateX(-15px)" },
+          "0%": { opacity: "0", transform: "translateX(-30px)" },
           "100%": { opacity: "1", transform: "translateX(0px)" },
         },
       },
